a87eb012b10315742ac4e819a0d4616c63f8c464
Brian342 <migelbrian3@gmail.com>
2024-07-17 09:35:00 +0300

    updated the counter file

--- Javascript/counter.js
+++ Javascript/counter.js
@@ -1,8 +1,12 @@
-let counter = 0;
+if (!localStorage.getItem('counter')){
+    localStorage.setItem('counter', 0);
+}
 
 function count(){
+    let counter = localStorage.getItem('counter');
     counter++;
     document.querySelector('h1').innerHTML = counter;
+    localStorage.setItem('counter', counter);
     
     // if (counter % 10 === 0) {
     //     alert(`Count is now ${counter}`); // `` they are know as template literal
@@ -12,6 +16,5 @@ function count(){
 document.addEventListener('DOMContentLoaded', function(){
     document.querySelector('button').onclick = count;
 
-    setInterval(count,1000);
 
 });//addEventListener takes 2 actions the  event and the function
