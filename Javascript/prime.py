1b17bfc36bc765e9002f5ff022abac877aa3f51a
Brian342 <migelbrian3@gmail.com>
2024-07-20 04:58:16 +0300

    testing if code has issues with python

---
+++ Javascript/prime.py
@@ -0,0 +1,10 @@
+import math
+
+def is_prime(n):
+    """This function check if a number is prime or not"""
+    if n < 2:
+        return False
+    for i in range(2, math.sqrt(n)):
+        if n % i == 0:
+            return False
+    return True
