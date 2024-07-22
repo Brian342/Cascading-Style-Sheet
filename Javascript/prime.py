96aca185dc2ca7a29ac967db2d9d91ecd5bda339
Brian342 <migelbrian3@gmail.com>
2024-07-22 08:11:21 +0300

    updated and type casted the for loop to have a int cast

--- Javascript/prime.py
+++ Javascript/prime.py
@@ -4,7 +4,7 @@ def is_prime(n):
     """This function check if a number is prime or not"""
     if n < 2:
         return False
-    for i in range(2, math.sqrt(n)):
+    for i in range(2, int(math.sqrt(n))):
         if n % i == 0:
             return False
     return True
