1e00b97a8c9b9603f1a959c9e99a963fd71f281f
Brian342 <migelbrian3@gmail.com>
2024-07-26 02:48:01 +0300

    added a code to check certain functions

--- Javascript/selenium/tests.py
+++ Javascript/selenium/tests.py
@@ -14,3 +14,25 @@ class WebpageTests(unittest.TestCase):
     def test_title(self):
         driver.get(file_uri("Sample.html"))
         self.assertEqual(driver.title, "Sample")
+    
+    def test_increase(self):
+        driver.get(file_uri("Sample.html"))
+        increase = driver.find_element_by_id("increase")
+        increase.click()
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "1")
+
+    def test_decrease(self):
+        driver.get(file_uri("Sample.html"))
+        decrease = driver.find_element_by_id("decrease")
+        decrease.click()
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "-1")
+
+    def test_multiple_increase(self):
+        driver.get(file_uri("Sample.html"))
+        increase = driver.find_element_by_id("increase")
+        for i in range(3):
+            increase.click()
+        self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
+
+if __name__ == "__main__":
+    unittest.main()
