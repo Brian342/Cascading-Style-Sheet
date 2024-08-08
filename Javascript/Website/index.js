4720deb3d9388b5f7b1ec149627d59c72a165aaa
Brian342 <migelbrian3@gmail.com>
2024-08-09 02:53:34 +0300

    Adding text content

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,2 +1,10 @@
-console.log(`Hello`);
-console.log(`I like pizza!`)
+// console.log(`Hello`);
+// console.log(`I like pizza!`);
+
+// // This is a pop up alert
+// window.alert(`This is an alert!`);
+// window.alert(`I like pizza`)
+
+// // This is a comment
+// /*
+// This is a multi-line comment */
