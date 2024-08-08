40480dcdfe02bb2498aae7121cbed67ba2254ab1
Brian342 <migelbrian3@gmail.com>
2024-08-09 02:59:00 +0300

    commented on the codes

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,13 +1,13 @@
 // console.log(`Hello`);
-// console.log(`I like pizza!`);
+// console.log(`I like pizza!`); // used for creating basic output
 
-// // This is a pop up alert
-// window.alert(`This is an alert!`);
+// // This is a pop up alert 
+// window.alert(`This is an alert!`); // creating a pop up alert
 // window.alert(`I like pizza`)
 
 // // This is a comment
 // /*
 // This is a multi-line comment */
 
-document.getElementById(`myH1`).textContent = `Hello`;
+document.getElementById(`myH1`).textContent = `Hello`; // used for changing Html content
 document.getElementById(`myP`).textContent = `I like Pizza`;
