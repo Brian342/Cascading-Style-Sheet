b8aef86f302ba0516425c7808102e836b9d2f62a
Brian342 <migelbrian3@gmail.com>
2024-08-13 19:37:05 +0300

    data type

--- Javascript/Website/variables.js
+++ Javascript/Website/variables.js
@@ -3,4 +3,15 @@
 
 // 1. declaration let x;
 // 2. assignment x = 100;
-let x;
+let x = 100;
+
+// console.log(x);
+// Number Data type
+let age = 25;
+let price = 10.99;
+let gpa = 2.1;
+
+console.log(typeof gpa); // used to dispay the datatype of variable
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your GPA is ${gpa}`);
