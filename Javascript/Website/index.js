0705336cf82578fd335bd584fb048837e341730e
Brian342 <migelbrian3@gmail.com>
2024-08-13 20:53:35 +0300

    Type conversion

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,8 +1,20 @@
 /*Type Conversion = change the datatype of a value to another
                     (Strings, numbers, boolean)*/
 
-let age = window.prompt(`How old are you?`);
-age = Number(age);
-age += 1;
+// let age = window.prompt(`How old are you?`);
+// age = Number(age);
+// age += 1;
 
-console.log(age, typeof age);
+// console.log(age, typeof age);
+
+let x = "";
+let y = "";
+let z = "";
+
+x = Number(x); // not a number
+y = String(y);
+z = Boolean(z); // returns a true or false
+
+console.log(x, typeof x);
+console.log(y, typeof y);
+console.log(z, typeof z); // true or false
