2a1fb67331762b42ce6fe8d26942b4b82e34736d
Brian342 <migelbrian3@gmail.com>
2024-08-13 19:57:00 +0300

    operators

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,13 +1,13 @@
-// console.log(`Hello`);
-// console.log(`I like pizza!`); // used for creating basic output
+/*arithmetic operators = operands (values, variables, etc)
+                        operators (+ - * /)
+                        eg. 11 = x + 5*/
 
-// // This is a pop up alert 
-// window.alert(`This is an alert!`); // creating a pop up alert
-// window.alert(`I like pizza`)
+let students = 50;
 
-// // This is a comment
-// /*
-// This is a multi-line comment */
-
-// document.getElementById(`myH1`).textContent = `Hello`; // used for changing Html content
-// document.getElementById(`myP`).textContent = `I like Pizza`;
+// students = student + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+students = students % 2;
+console.log(students);
