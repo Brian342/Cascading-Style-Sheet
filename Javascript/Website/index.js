70bdd2cf8849bf789df8d594fde173d78cacab23
Brian342 <migelbrian3@gmail.com>
2024-08-13 20:16:03 +0300

    user input

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,3 +1,10 @@
 /* How to accept user input
 1. Easy way = window prompt
 2. Proffessional way = HTML textbox*/
+
+// 1. Easy way
+let userName;
+
+username = window.prompt("What's your username?")
+
+console.log(username)
