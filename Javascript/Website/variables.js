1311ef23f2933ae055ef6bc0643f47bb3f849af3
Brian342 <migelbrian3@gmail.com>
2024-08-13 19:51:38 +0300

    data types

--- Javascript/Website/variables.js
+++ Javascript/Website/variables.js
@@ -1,26 +1,10 @@
 /* Variables = A container that stores a value 
                 behaves as if it were the value it contains*/
 
-// 1. declaration let x;
-// 2. assignment x = 100;
-let x = 100;
+let fullName = "Brian Kim";
+let age = 22;
+let student = true;
 
-// console.log(x);
-// Number Data type
-// let age = 25;
-// let price = 10.99;
-// let gpa = 2.1;
-
-// Strings
-// let firstName = "Bro"
-// let favoriteFood = "Pizza"
-// let email = "Bro@gmail.com"
-
-// Boolean
-let online = false;
-let forsale = true;
-let isstudent = true
-
-console.log(`Bro is online ${online}`);
-console.log(`Is this car for sale: ${forsale}`);
-console.log(`Enrolled: ${isstudent}`)
+document.getElementById("p1").textContent = `Your Name is ${fullName}`;
+document.getElementById("p2").textContent = `You are ${age} years old`;
+document.getElementById("p3").textContent = `Enrolled at school ${student}`;
