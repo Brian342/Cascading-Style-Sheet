f88cc585e63b00d99b3cd8c021b3f2dc50d2b17f
Brian342 <migelbrian3@gmail.com>
2024-08-14 22:24:06 +0300

    count function

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,2 +1,19 @@
 /* Counter Program
 */
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetbtn");
+const increaseBtn = document.getElementById("increasebtn");
+const countLabel = document.getElementById("countLabel");
+let count = 0;
+
+//increase
+increaseBtn.onclick = function(){
+    count++;
+    countLabel.textContent = count;
+}
+
+//decrease
+decreaseBtn.onclick = function(){
+    count--;
+    countLabel.textContent = count;
+}
