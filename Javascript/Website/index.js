9e7ba8974d6414e60ff5c53daf49230eede24658
Brian342 <migelbrian3@gmail.com>
2024-08-14 22:49:07 +0300

    math function

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,2 +1,22 @@
 /* Math - built in object that provides a 
         collection of properties and methods*/
+
+// console.log(Math.PI)
+// console.log(Math.E)
+
+
+let x = 3.23
+let y = 2
+let z;
+
+// z = Math.round(x)
+// z = Math.floor(x)
+// z = Math.ceil(x)
+// z = Math.abs(x)
+// z = Math.pow(x, y)
+// z = Math.sqrt(x)
+// z = Math.max(x, y)
+// z = Math.min(x, y)
+
+z = Math.random()
+console.log(z)
