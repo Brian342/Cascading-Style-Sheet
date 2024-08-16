ec9a4b5bc50883a5875083901ca0e3dc00b65e8d
Brian342 <migelbrian3@gmail.com>
2024-08-17 01:49:01 +0300

    if statements

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,22 +1,2 @@
-/*Random Number generator */
-
-const myButton = document.getElementById("myButton");
-const myLabel1= document.getElementById("Label1");
-const myLabel2= document.getElementById("Label2");
-const myLabel3= document.getElementById("Label3");
-
-const min = 1;
-const max = 6;
-let randomnum1;
-let randomnum2;
-let randomnum3;
-
-myButton.onclick = function() {
-        randomnum1 = Math.floor(Math.random() * max) + min;
-        randomnum2 = Math.floor(Math.random() * max) + min;
-        randomnum3 = Math.floor(Math.random() * max) + min;
-        Label1.textContent = randomnum1;
-        Label2.textContent = randomnum2;
-        Label3.textContent = randomnum3;
-
-}
+/* If Statements - if a condition is true, execute some code
+                if not, do something */
