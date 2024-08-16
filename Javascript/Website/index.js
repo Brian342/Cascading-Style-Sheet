2269fe2dee6f8e319bb5d1ef440a7f37c910ce6b
Brian342 <migelbrian3@gmail.com>
2024-08-17 01:51:10 +0300

    If statements

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,2 +1,12 @@
 /* If Statements - if a condition is true, execute some code
                 if not, do something */
+
+
+let age = 25;
+
+if (age > 18) {
+        console.log("You are an adult");
+}
+else {
+        console.log("You must be 18+ to enter this site");
+}
