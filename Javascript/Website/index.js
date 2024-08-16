ddc4635b752253eac5e2156fbd61330db2f33b8f
Brian342 <migelbrian3@gmail.com>
2024-08-17 01:45:05 +0300

    Random number generator

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,13 +1,22 @@
 /*Random Number generator */
 
 const myButton = document.getElementById("myButton");
-const myLabel = document.getElementById("myLabel");
+const myLabel1= document.getElementById("Label1");
+const myLabel2= document.getElementById("Label2");
+const myLabel3= document.getElementById("Label3");
 
 const min = 1;
 const max = 6;
-let randomnum;
+let randomnum1;
+let randomnum2;
+let randomnum3;
 
 myButton.onclick = function() {
-        randomnum = Math.floor(Math.random() * max) + min;
-        myLabel.textContent = randomnum;
+        randomnum1 = Math.floor(Math.random() * max) + min;
+        randomnum2 = Math.floor(Math.random() * max) + min;
+        randomnum3 = Math.floor(Math.random() * max) + min;
+        Label1.textContent = randomnum1;
+        Label2.textContent = randomnum2;
+        Label3.textContent = randomnum3;
+
 }
