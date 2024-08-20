b75dc045aa095c01b244b97d869853a56f22475a
Brian342 <migelbrian3@gmail.com>
2024-08-21 00:59:43 +0300

    string methods

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -17,3 +17,7 @@ console.log(userName.substring(0, 3)) // used to get a part of a string
 console.log(userName.slice(0, 3)) // used to get a part of a string
 let result = userName.startsWith(" ");
 console.log(result)
+
+let phoneNumber = "123-456-789";
+phoneNumber = phoneNumber.replaceAll("-", "")
+console.log(phoneNumber);
