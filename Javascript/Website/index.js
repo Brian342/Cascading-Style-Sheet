1f531c534a368419f09eeab3b19f9c3e0a9d7535
Brian342 <migelbrian3@gmail.com>
2024-08-21 00:16:36 +0300

    Ternary operator

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,7 +1,15 @@
 /* Ternary operator = a shortcut to if{} and else{} statements
 helps to assign a variable based on a condition
 condition ? codeiftrue : codeiffalse */
-let age = 21;
+// let age = 21;
 
-let message = age >= 18 ? `you're an adult` : `you're a minor`;
-console.log(message);
+// let message = age >= 18 ? `you're an adult` : `you're a minor`;
+// console.log(message);
+
+// let time = 16;
+// let greeting = time < 12 || time > 18 ? 'Good morning' : `Good afternoon`;
+// console.log(greeting); // Good afternoon
+
+let isstudent = true;
+let message = isstudent ? `You are a student` : `You are not a student`;
+console.log(message); // You are a student
