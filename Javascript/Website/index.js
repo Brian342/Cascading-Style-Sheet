000628548c89b6e2645fc53ee46e686987d6d058
Brian342 <migelbrian3@gmail.com>
2024-08-21 00:54:56 +0300

    strings in javascript

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,27 +1,14 @@
-/*Switch = can  be an efficient replacement to many else if statements */
+/* Strings methods = allow you to manipulate and work with text (strings) */
 
-let textScore = 92;
-let letterGrade;
+let userName = "BroCode";
 
-switch(true){
-        case textScore >= 90:
-                letterGrade = "A";
-                break;
-        case textScore >= 80:
-                letterGrade = "B";
-                break;
-        case textScore >= 70:
-                letterGrade = "C";
-                break;
-        case textScore >= 60:
-                letterGrade = "D";
-                break;
-        case textScore >= 50:
-                letterGrade = "E";
-                break;
-        default:
-                letterGrade = "F";
-                break;
-        
-}
-console.log(letterGrade); // Output: A
+console.log(userName.charAt(0)) // used to get a specific value 
+console.log(userName.charCodeAt(0)) // used to get the ASCII value of a character
+console.log(userName.indexOf("o")) // used to get the index of a specific value
+console.log(userName.lastIndexOf("o")) // used to get the last index of a specific value
+console.log(userName.length) // used to get the length of a string
+console.log(userName.localeCompare("BroCode")) // used to compare two strings
+console.log(userName.toLowerCase()) // used to convert a string to lowercase
+console.log(userName.toUpperCase()) // used to convert a string to uppercase
+console.log(userName.trim()) // used to remove spaces from a string
+console.log(userName.replace("Bro", "Brother")) // used to replace a value in a string
