7621c4c25a696f9b7bfc757e6167b5feafe31cac
Brian342 <migelbrian3@gmail.com>
2024-08-20 23:34:32 +0300

    checked project

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,2 +1,18 @@
 /* Checked = property that determines the checked state of an
         HTML checkbox or radio button element */
+const myCheckBox = document.getElementById("myCheckBox");
+const visaBtn = document.getElementById("visaBtn");
+const MastercardBtn = document.getElementById("MastercardBtn");
+const paypalBtn = document.getElementById("paypalBtn");
+const mySubmit = document.getElementById("mySubmit");
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
+
+mySubmit.onclick = function(){
+       if(myCheckBox.checked){
+        subResult.textContent = `You are subscribed`;
+       } 
+       else{
+        subResult.textContent = `You are not subscribed`;
+       }
+}
