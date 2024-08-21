1d3bdaa34b6f47838c2af8da4b2c1a23c5e10766
Brian342 <migelbrian3@gmail.com>
2024-08-21 21:57:07 +0300

    Method chaining

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,26 +1,9 @@
-/*String slicing = creating a substring from a 
-        portion of another string
-        string.slice(start, end)
- */
+/* Method chaining = calling one method after another
+in one continuous line of code
 
-// const fullName = "Broseph code"
+*/
 
-// let firstName = fullName.slice(0, 7);
-// let lastName = fullName.slice(4, 8);
+// No method chaning
 
-// let firstChar = fullName.slice(0, 1);
-// let lastChar = fullName.slice(-1)
+let userName = window.prompt();
 
-// let firstName = fullName.slice(0, fullName.indexOf(" "))
-// let lastName = fullName.slice(fullName.indexOf(" ")+ 1)
-// console.log(firstName);
-// console.log(lastName);  
-// console.log(lastName);
-// console.log(firstChar);
-// console.log(lastChar);  //lastChar = fullName.slice(-1) = last character of the
-
-const email = "brocode@gmail.com";
-let username = email.slice(0, email.indexOf("@"));
-let extension = email.slice(email.indexOf("@")+1)
-console.log(extension);
-console.log(username)
