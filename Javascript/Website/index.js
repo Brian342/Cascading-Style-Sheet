3aea5b345ca922b3223e45f2f6fec524eb207026
Brian342 <migelbrian3@gmail.com>
2024-08-21 21:45:23 +0300

    string slicing

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -3,7 +3,24 @@
         string.slice(start, end)
  */
 
-const fullName = "Bro code"
+// const fullName = "Broseph code"
 
-let firstName = fullName.slice(0, 2)
-console.log(firstName)
+// let firstName = fullName.slice(0, 7);
+// let lastName = fullName.slice(4, 8);
+
+// let firstChar = fullName.slice(0, 1);
+// let lastChar = fullName.slice(-1)
+
+// let firstName = fullName.slice(0, fullName.indexOf(" "))
+// let lastName = fullName.slice(fullName.indexOf(" ")+ 1)
+// console.log(firstName);
+// console.log(lastName);  
+// console.log(lastName);
+// console.log(firstChar);
+// console.log(lastChar);  //lastChar = fullName.slice(-1) = last character of the
+
+const email = "brocode@gmail.com";
+let username = email.slice(0, email.indexOf("@"));
+let extension = email.slice(email.indexOf("@")+1)
+console.log(extension);
+console.log(username)
