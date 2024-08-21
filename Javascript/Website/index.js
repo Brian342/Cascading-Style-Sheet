e28ffc97370010a1cb045f1a1e14f3969e15ca1b
Brian342 <migelbrian3@gmail.com>
2024-08-21 22:27:36 +0300

    Method chaining

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -5,14 +5,19 @@ in one continuous line of code
 
 // No method chaning
 
-let userName = window.prompt("Enter your username! ");
-userName = userName.trim();
-let letter = userName.charAt(0);
-letter = letter.toUpperCase();
+// let userName = window.prompt("Enter your username! ");
+// userName = userName.trim();
+// let letter = userName.charAt(0);
+// letter = letter.toUpperCase();
 
-let extraChars = userName.slice(1);
-extraChars = extraChars.toLowerCase();
-userName = letter + extraChars;
+// let extraChars = userName.slice(1);
+// extraChars = extraChars.toLowerCase();
+// userName = letter + extraChars;
 
-console.log(userName);
+// console.log(userName);
+
+// Method Chaining
+let username = window.prompt("Enter your username! ");
+username = username.trim().charAt(0).toUpperCase() + username.trim().slice(1).toLowerCase();
+console.log(username)
 
