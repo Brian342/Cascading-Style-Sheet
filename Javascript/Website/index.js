dab49a5bf66bfe95422aaaec80ade65faffe4b90
Brian342 <migelbrian3@gmail.com>
2024-08-21 22:49:41 +0300

    strict operator

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -3,3 +3,13 @@
  === strict equality operator (compare if values & datatype are equal) 
  != inequality operator
  !== strict inequality operator*/
+
+const PI = 3.14;
+
+if (PI === 3.14) {
+        console.log("That is PI")
+        
+}
+else{
+        console.log("That is not PI")
+}
