b9fd156128e886e0f5630cde88d55eb400671c6c
Brian342 <migelbrian3@gmail.com>
2024-08-22 22:20:32 +0300

    while loop

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,8 +1,21 @@
 /* while loop = repeat some code WHILE some condition is True*/
-let userName = "";
+// let userName = "";
 
-while(userName === "" || userName === null){
-        userName = window.prompt("Enter your Name")
+// while(userName === "" || userName === null){
+//         userName = window.prompt("Enter your Name")
+// }
+
+// // do{
+// //         userName = window.prompt("Enter your Name");
+// // }while(userName === "" || userName === null)
+
+// console.log(`Hello ${userName}`)  
+
+let loggedIn = false;
+let username;
+let password;
+
+while (!loggedIn) {
+        username = window.prompt("Enter your username");
+        
 }
-
-console.log(`Hello ${userName}`)  
