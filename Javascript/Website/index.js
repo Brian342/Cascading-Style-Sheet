06ad88fc0d2c0969bfbeec2c9d2acfc5b0887522
Brian342 <migelbrian3@gmail.com>
2024-08-22 22:25:02 +0300

    while loops

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -17,5 +17,17 @@ let password;
 
 while (!loggedIn) {
         username = window.prompt("Enter your username");
-        
+        password = window.prompt("Enter your password");
+
+        if(username === "myusername" && password === "mypassword"){
+                loggedIn = true;
+                console.log(`You are logged in! enjoy`)
+        }
+        else if(username === false){
+                console.log(`Incorrect username`)
+        }
+        else{
+                console.log(`Incorrect password`)
+        }
+         
 }
