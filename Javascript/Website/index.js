4efac93a04f609352bb432372d8cd862922f6932
Brian342 <migelbrian3@gmail.com>
2024-08-22 22:38:32 +0300

    for loops

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,33 +1,6 @@
-/* while loop = repeat some code WHILE some condition is True*/
-// let userName = "";
+/*For loops = repeat some code a LIMITED amount of times */
 
-// while(userName === "" || userName === null){
-//         userName = window.prompt("Enter your Name")
-// }
+for(let i = 0; i <= 10; i++){
+        console.log(i);
 
-// // do{
-// //         userName = window.prompt("Enter your Name");
-// // }while(userName === "" || userName === null)
-
-// console.log(`Hello ${userName}`)  
-
-let loggedIn = false;
-let username;
-let password;
-
-do{
-        username = window.prompt("Enter your username");
-        password = window.prompt("Enter your password");
-
-        if(username === "myusername" && password === "mypassword"){
-                loggedIn = true;
-                console.log(`You are logged in! enjoy`);
-        }
-        else if(username !== "myusername" ){
-                console.log(`Incorrect username`);
-        }
-        else{
-                console.log(`Incorrect password`);
-        }
-         
-}while (!loggedIn)
+}
