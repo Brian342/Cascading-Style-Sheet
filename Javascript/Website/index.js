d2ce641324607aec864794fdecd506d1e82c8594
Brian342 <migelbrian3@gmail.com>
2024-08-22 22:41:31 +0300

    for loops

--- Javascript/Website/index.js
+++ Javascript/Website/index.js
@@ -1,6 +1,7 @@
 /*For loops = repeat some code a LIMITED amount of times */
 
-for(let i = 0; i <= 10; i++){
+for(let i = 10; i > 0; i--){
         console.log(i);
 
 }
+console.log(`Happy new year!!!!!`)
